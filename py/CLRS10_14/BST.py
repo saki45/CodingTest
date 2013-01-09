fe289cb25131212665372f8b89e34bc22147c608
Yume Asakura <=>
2013-01-08 19:50:29 -0500

    bst

---
+++ py/CLRS10_14/BST.py
@@ -0,0 +1,11 @@
+class BinarySearchTreeNode:
+	def __init__(self, key = 0):
+		self.key = key
+		self.ltree = None
+		self.rtree = None
+
+class BinarySearchTree:
+	def __init__(self, root = None):
+		self.root = root;
+
+	
